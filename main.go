b36ed308a35c241da5e2397ee574a4b63c3bb720
Fadzaa <fadza20@gmail.com>
2024-01-23 23:03:43 +0700

    add database connection

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-gin-api/handler/coffee"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/coffee_api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database Connected")
+
 	router := gin.Default()
 
 	router.GET("/", coffee.RootHandler)
@@ -14,7 +25,7 @@ func main() {
 	c.GET("/", coffee.GetCoffeeHandler)
 	c.POST("/", coffee.CreateCoffeeHandler)
 
-	err := router.Run(":8080")
+	err = router.Run(":8080")
 	if err != nil {
 		panic(err)
 	}
