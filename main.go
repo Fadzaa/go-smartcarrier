840272b21caba151d455cb1a6b7d212906f2079f
Fadzaa <fadza20@gmail.com>
2024-01-23 23:26:04 +0700

    add auto migration

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"go-gin-api/handler/coffee"
+	"go-gin-api/domains/coffee"
+	coffeeHandler "go-gin-api/handlers/coffee"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/coffee_api?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&coffee.Coffee{})
 	if err != nil {
 		panic(err)
 	}
@@ -19,11 +25,11 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/", coffee.RootHandler)
+	router.GET("/", coffeeHandler.RootHandler)
 
 	c := router.Group("/coffee")
-	c.GET("/", coffee.GetCoffeeHandler)
-	c.POST("/", coffee.CreateCoffeeHandler)
+	c.GET("/", coffeeHandler.GetCoffeeHandler)
+	c.POST("/", coffeeHandler.CreateCoffeeHandler)
 
 	err = router.Run(":8080")
 	if err != nil {
