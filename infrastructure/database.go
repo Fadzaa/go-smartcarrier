2167ac653eb8b4daef9ae0453400084a02335a09
Fadzaa <fadza20@gmail.com>
2024-01-27 03:41:34 +0700

    add env

--- infrastructure/database.go
+++ infrastructure/database.go
@@ -2,13 +2,16 @@ package infrastructure
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"go-gin-api/domain/auth"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 func ConnectToDatabase() *gorm.DB {
-	dsn := "root:@tcp(127.0.0.1:3306)/api_golang?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	dsn := os.Getenv("DB")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
