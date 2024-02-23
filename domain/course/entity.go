ed52b3ea0109656a5d0f4341b9cb1359a6ee6551
Fadzaa <fadza20@gmail.com>
2024-02-24 00:53:40 +0700

    fix course struct

--- domain/course/entity.go
+++ domain/course/entity.go
@@ -1,19 +1,13 @@
 package course
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
-
 type Course struct {
-	gorm.Model
-	ID          int8      `json:"id" gorm:"primaryKey"`
-	ImageCourse string    `json:"image_course"`
-	Title       string    `json:"title" gorm:"type:varchar(100)"`
-	Description string    `json:"description"`
-	Percentage  int8      `json:"percentage"`
-	Difficulty  string    `json:"difficulty" gorm:"type:varchar(20)"`
-	Category    string    `json:"category" gorm:"type:varchar(20)"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int8   `json:"id" gorm:"primaryKey"`
+	ImageCourse string `json:"image_course"`
+	Title       string `json:"title" gorm:"type:varchar(100)"`
+	Description string `json:"description"`
+	//Percentage  int8      `json:"percentage"`
+	//Difficulty  string    `json:"difficulty" gorm:"type:varchar(20)"`
+	//Category    string    `json:"category" gorm:"type:varchar(20)"`
+	//CreatedAt   time.Time `json:"created_at"`
+	//UpdatedAt   time.Time `json:"updated_at"`
 }
