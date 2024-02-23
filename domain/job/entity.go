1e00c7a75ce23c5bb58b356ff81de398ce57aa14
Fadzaa <fadza20@gmail.com>
2024-02-24 01:33:44 +0700

    fix post job

--- domain/job/entity.go
+++ domain/job/entity.go
@@ -1,13 +1,15 @@
 package job
 
+import "time"
+
 type Job struct {
 	//gorm.Model
-	ID          int8   `json:"id" gorm:"primaryKey"`
-	CompanyName string `json:"company_name" gorm:"type:varchar(255)"`
-	Jobdesk     string `json:"jobdesk" gorm:"type:varchar(100)"`
-	Location    string `json:"location" gorm:"type:varchar(100)"`
-	//JobType      int8      `json:"job_type"`
-	CompanyImage string `json:"company_image"`
-	//CreatedAt    time.Time `json:"created_at"`
-	//UpdatedAt    time.Time `json:"updated_at"`
+	ID           int8      `json:"id" gorm:"primaryKey"`
+	CompanyName  string    `json:"company_name" gorm:"type:varchar(255)"`
+	Jobdesk      string    `json:"jobdesk" gorm:"type:varchar(100)"`
+	Location     string    `json:"location" gorm:"type:varchar(100)"`
+	JobType      int8      `json:"job_type"`
+	CompanyImage string    `json:"company_image"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
