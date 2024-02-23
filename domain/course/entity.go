060f855d19f3a90e30cc459d00f570e1d9bfeb46
Fadzaa <fadza20@gmail.com>
2024-02-24 00:50:47 +0700

    fix entities

--- domain/course/entity.go
+++ domain/course/entity.go
@@ -7,18 +7,13 @@ import (
 
 type Course struct {
 	gorm.Model
-	ID          int8   `json:"id" gorm:"primaryKey"`
-	ImageCourse string `json:"image_course"`
-	Title       string `json:"title" gorm:"type:varchar(100)"`
-	Description string `json:"description"`
-	Percentage  int8   `json:"percentage"`
-	Difficulty  string `json:"difficulty" gorm:"type:varchar(20)"`
-	Category    string `json:"category" gorm:"type:varchar(20)"`
-	CourseDesc  CourseDeciription
+	ID          int8      `json:"id" gorm:"primaryKey"`
+	ImageCourse string    `json:"image_course"`
+	Title       string    `json:"title" gorm:"type:varchar(100)"`
+	Description string    `json:"description"`
+	Percentage  int8      `json:"percentage"`
+	Difficulty  string    `json:"difficulty" gorm:"type:varchar(20)"`
+	Category    string    `json:"category" gorm:"type:varchar(20)"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
-}
-
-type CourseDeciription struct {
-	Name string
 }
