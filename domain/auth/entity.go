3345a8244d60615fd68c3285865448af44e4227d
Fadzaa <fadza20@gmail.com>
2024-02-24 01:07:32 +0700

    fix auth entities

--- domain/auth/entity.go
+++ domain/auth/entity.go
@@ -1,22 +1,13 @@
 package auth
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
-
 type User struct {
-	gorm.Model
-
-	ID          int8      `json:"id"`
-	Name        string    `json:"name" gorm:"size:255" gorm:"not null"`
-	Email       string    `json:"email" gorm:"size:100"`
-	Password    string    `json:"password"`
-	PhoneNumber string    `json:"phone_number" gorm:"size:50"`
-	Role        string    `json:"role" gorm:"size:50"`
-	Gender      int8      `json:"gender"`
-	Province    string    `json:"province"`
-	City        string    `json:"city"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int8   `json:"id"`
+	Name        string `json:"name" gorm:"size:255" gorm:"not null"`
+	Email       string `json:"email" gorm:"size:100"`
+	Password    string `json:"password"`
+	PhoneNumber string `json:"phone_number" gorm:"size:50"`
+	Role        string `json:"role" gorm:"size:50"`
+	Gender      int8   `json:"gender"`
+	Province    string `json:"province"`
+	City        string `json:"city"`
 }
